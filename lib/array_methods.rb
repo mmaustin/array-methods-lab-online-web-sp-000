db84c4302de478c985fdd05a9504f4834845149d
mccray austin <tubach75@hotmail.com>
2020-09-19 14:22:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,17 +9,21 @@ end
 using_sort(["wow", "I", "am", "really", "learning", "arrays"])
 
 def using_reverse(array)
-
+  array.reverse
 end
+using_reverse(["wow", "I", "am", "really", "learning", "arrays"])
 
 def using_first(array)
-
+  array.first
 end
+using_first(["wow", "I", "am", "really", "learning", "arrays"])
 
 def using_last(array)
-
+  array.last
 end
+using_last(["wow", "I", "am", "really", "learning", "arrays"])
 
 def using_size(array)
-
+  array.size
 end
+using_size(["wow", "I", "am", "really", "learning", "arrays"])
